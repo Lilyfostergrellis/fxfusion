28054135055cae306627095f34a9d62488ba2f9a
fltdr <florin666@gmail.com>
2023-02-03 09:29:23 +0000

    small changes to newsapi code

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Latest news API from gnews.io. Note limit of maximum 100 requests per day.
 
-const key = "d6a5e9d948e4965654dfa70f78b94ca0"
-const url = `https://gnews.io/api/v4/search?q=example&token=${key}`
+let key = "d6a5e9d948e4965654dfa70f78b94ca0"
+let url = `https://gnews.io/api/v4/search?q=example&token=${key}`
 
 // Generate required HTML usint createElement
 
-const recievedNews = (newsdata) => {
-    const articlesDiv = document.querySelector(".articles")
+let recievedNews = (newsdata) => {
+    let articlesDiv = document.querySelector(".articles")
     newsdata.articles.forEach((article) => {
-      const div = document.createElement("div")
+      let div = document.createElement("div")
       div.className = "news"
       div.innerHTML = `
 			<h2>${article.title}</h2>
