3111be9601cb7f67b66a5fe7cffd0b72de795736
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-02-07 17:18:50 +0000

    corrected url call

--- script.js
+++ script.js
@@ -7,7 +7,8 @@
 let apikey = 'd6a5e9d948e4965654dfa70f78b94ca0';
 let category = 'business';
 //The catagory can be changed to one of five, see API documentation for other options
-let url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&apikey=' + apikey + '&lang=en&country=uk&max=6';
+let url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&apikey=' + apikey + '&lang=en&country=en&max=6';
+//language english, country of choice for top trending google articles is england, maximum results returned is 6.
 
 fetch(url)
   .then(function (response) {
@@ -28,4 +29,14 @@ fetch(url)
       // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
     }
   });
+
+function renderNewsResults(dailyNews){
+
+
+};
+//new function to display the top daily data
+
+
+
+
 
